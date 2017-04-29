32359ec361ec7d9bdd54249977e800b21594ab2f
German Velasco <germsvel@gmail.com>
2017-04-29 15:17:39 -0400

    Update Alf.Storage to include mix environment in filename

--- lib/alf/storage.ex
+++ lib/alf/storage.ex
@@ -4,14 +4,14 @@ defmodule Alf.Storage do
   def store(text, filename) do
     ensure_directory_is_created()
 
-    {:ok, file} = File.open(@dir_name <> filename, [:write])
+    {:ok, file} = File.open(storage_name(filename), [:write])
 
     IO.write(file, text)
     File.close(file)
   end
 
   def get(filename) do
-    case File.read(@dir_name <> filename) do
+    case File.read(storage_name(filename)) do
       {:ok, text} -> text
       _ -> ""
     end
@@ -19,6 +19,10 @@ defmodule Alf.Storage do
 
   def clear_records do
     File.rm_rf(@dir_name)
+  end
+
+  defp storage_name(filename) do
+    @dir_name <> filename <> Atom.to_string(Mix.env)
   end
 
   defp ensure_directory_is_created do
